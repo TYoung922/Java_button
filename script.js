d49200965cf9e9589893cc5daaa16b46a00cf698
TercanoNuruva <thyoung89@gmail.com>
2024-05-28 10:23:20 -0600

    sliders

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function btnPressed() {
   btnclick += 1;
   clicks.innerHTML = +btnclick;
 }
+
+function changeColor() {
+  let red = document.getElementById("rangeRed").value;
+  let blue = document.getElementById("rangeBlue").value;
+  let green = document.getElementById("rangeGreen").value;
+  let color = "rgb(" + red + "," + green + "," + blue + ")";
+  document.body.style.backgroundColor = color;
+}
+
+document.getElementById("rangeRed").addEventListener("input", changeColor);
+document.getElementById("rangeBlue").addEventListener("input", changeColor);
+document.getElementById("rangeGreen").addEventListener("input", changeColor);
